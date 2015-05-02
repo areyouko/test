dc1bca18130dff034c5852f0a4192acbd992998d
jongdae park <jongdaepark@jongdaeui-MacBook.local>
2015-05-02 14:18:50 +0900

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by jongdae park on 2015. 5. 2..
+//  Copyright (c) 2015년 ___JongDaePark___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
